e79d8a14580a462a52e9b4058f25fd9bd521c5d4
samagragupta <samagraguptajpr@gmail.com>
2018-12-25 18:37:43 +0530

    Working app

--- main.py
+++ main.py
@@ -11,4 +11,16 @@ def downloader():
 	destination="E:\Movies"
 	stream.download(destination)
 
+title=tk.Label(text="Welcome to YouTube Downloader.",font=40)
+title.grid(column=0,row=0,padx=10, pady=10)
+
+label=tk.Label(text="Please Enter the link of the video")
+label.grid(column=0,row=1,padx=10, pady=10)
+
+entry=tk.Entry()
+entry.grid(column=0,row=2)
+
+button=tk.Button(text="Click Here",font=15,bg="red",command=downloader)
+button.grid(column=0,row=3,padx=10, pady=10)
+
 window.mainloop()
